2ab6a831a7c79f48970b8b9f89708b4b704bc1b1
Anwesh43 <anweshthecool0@gmail.com>
2019-01-20 12:19:58 +0530

    adding libraries and defining constants

--- bisidearrowview/src/main/java/com/anwesh/uiprojects/bisidearrowview/BiSideArrowView.kt
+++ bisidearrowview/src/main/java/com/anwesh/uiprojects/bisidearrowview/BiSideArrowView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bisidearrowview
 /**
  * Created by anweshmishra on 20/01/19.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
