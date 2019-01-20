6d925e38f5e4839eefc763d1859a4b8e1aeff648
Anwesh43 <anweshthecool0@gmail.com>
2019-01-20 13:05:22 +0530

    adding BiSideArrow class

--- bisidearrowview/src/main/java/com/anwesh/uiprojects/bisidearrowview/BiSideArrowView.kt
+++ bisidearrowview/src/main/java/com/anwesh/uiprojects/bisidearrowview/BiSideArrowView.kt
@@ -179,4 +179,28 @@ class BiSideArrowView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiSideArrow(var i : Int) {
+
+        private val root : BSANode = BSANode(0)
+        private var curr : BSANode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
